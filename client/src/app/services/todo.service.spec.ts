c618afc4608cbf6a0b67f305fa8c9fa22670bd29
bjorndecraemer <bjorn.decraemer@yahoo.com>
2018-12-27 12:30:16 +0100

    Added getAllEnhancedTodos IT

--- client/src/app/services/todo.service.spec.ts
+++ client/src/app/services/todo.service.spec.ts
@@ -14,6 +14,8 @@ import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 import {EffectsModule} from "@ngrx/effects";
 import {StoreRouterConnectingModule} from "@ngrx/router-store";
 import {GiphyService} from "./giphy.service";
+import {Todo} from "../todo/model/todo";
+import {of} from "rxjs";
 
 describe('TodoService', () => {
 
@@ -27,30 +29,71 @@ describe('TodoService', () => {
     {path: "**",
       redirectTo: '/todos'}
   ];
+  let todoService : TodoService;
+  beforeEach(() => {
+      TestBed.configureTestingModule(
+        {
+          declarations: [
+            AppComponent,
+            HomeComponent,
+            NavbarComponent
+          ],
+          imports: [
+            BrowserModule,
+            RouterModule.forRoot(routes),
+            HttpClientModule,
+            StoreModule.forRoot(reducers, {metaReducers}),
+            !environment.production ? StoreDevtoolsModule.instrument() : [],
+            EffectsModule.forRoot([]),
+            StoreRouterConnectingModule.forRoot({stateKey: 'router'})
+          ],
+          providers: [TodoService, GiphyService]
+        }
+      );
+      todoService = TestBed.get(TodoService);
+    }
 
-  beforeEach(() => TestBed.configureTestingModule(
+  );
 
-
-    {declarations: [
-        AppComponent,
-        HomeComponent,
-        NavbarComponent
-      ],
-      imports: [
-        BrowserModule,
-        RouterModule.forRoot(routes),
-        HttpClientModule,
-        StoreModule.forRoot(reducers, {metaReducers}),
-        !environment.production ? StoreDevtoolsModule.instrument() : [],
-        EffectsModule.forRoot([]),
-        StoreRouterConnectingModule.forRoot({stateKey: 'router'})
-      ],
-      providers: [TodoService, GiphyService]
-      }
-    ));
 
   it('should be created', () => {
     const service: TodoService = TestBed.get(TodoService);
     expect(service).toBeTruthy();
   });
+
+  describe('getAllEnhancedTodos', () => {
+    it('should return a collection of Todos', function () {
+      let todo1 : Todo =  {
+        completed: true,
+        completedDate: undefined,
+        createdDate: undefined,
+        description: 'my description',
+        giphyUrl: "",
+        id: 1,
+        title: "my title"
+      }
+      let todo2 : Todo =  {
+        completed: true,
+        completedDate: undefined,
+        createdDate: undefined,
+        description: 'my description2',
+        giphyUrl: "",
+        id: 2,
+        title: "my title2"
+      }
+
+      let todoList  =  {
+        todolist : [todo1,todo2]
+      }
+
+      let response;
+      spyOn(todoService,'getAllEnhancedTodos').and.returnValue(of(todoList));
+
+      todoService.getAllEnhancedTodos().subscribe(res => {
+        response = res;
+      });
+
+      expect(response).toEqual(todoList);
+    });
+  })
 });
